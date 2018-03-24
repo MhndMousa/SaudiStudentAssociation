97b920ebee74e47c695c4b36a8557900c9e145fc
Mhnd Mousa <mhndmsa@gmail.com>
2018-03-24 14:56:53 -0400

    Created class to hold the information of the data fetched from firebase

---
+++ SSUAkron/CardInformation.swift
@@ -0,0 +1,16 @@
+//
+//  CardInformation.swift
+//  SSUAkron
+//
+//  Created by Muhannad Mousa on 3/24/18.
+//  Copyright © 2018 Muhannad Mousa. All rights reserved.
+//
+
+import UIKit
+
+class CardInformaion: NSObject {
+    var title: Int?
+    var itemTitle: Int?
+    var itemSubtitle: Int?
+
+}
