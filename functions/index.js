edcad8cdafeb7857e9bd30b4c0983cd83b589933
Mhnd Mousa <mhndmsa@gmail.com>
2018-04-07 23:16:53 -0400

    added cloud functions to project and created a function that creates a user refrence in database upon creating an account

---
+++ functions/index.js
@@ -0,0 +1,24 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase)
+const ref = admin.database().ref()
+
+
+
+exports.createUserAccount = functions.auth.user().onCreate(event =>{
+  // data coming from the function comes in event.data
+
+  const user = event.data
+
+  const uid = user.uid
+  const email = user.email
+  const displayName = user.displayName || ""
+
+  // Creates new user in Database
+  const newUserRef = ref.child(`/users/${uid}`)
+  return newUserRef.set({
+    email: email,
+    displayName: displayName,
+    // password: password
+  })
+})
